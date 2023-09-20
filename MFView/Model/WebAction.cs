54864c8ec1d1ab4711df30e0b731540c05a1a9d1
Zachary Hartman <z.hartman86@live.com>
2023-09-19 22:48:37 -0400

    updated csheet

---
+++ MFView/Model/WebAction.cs
@@ -0,0 +1,9 @@
+﻿namespace MFView
+{
+    public class WebAction
+    {
+        public string Source { get; set; } = "";
+        public string Action { get; set; } = "";
+        public string Target { get; set; } = "";
+    }
+}
